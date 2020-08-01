53f486f5b03937c3ab48546f2ccb46a591805402
Mark Sun <sunmark14@gmail.com>
2020-08-01 06:51:35 -0400

    adding all our new blog code for gatsby setup

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const ContactPage = () => (
+  <main>
+    <h1>Contact Info</h1>
+    <p>Address: 123 HappyStreet Dr, Anytown MI USA</p>
+  </main>
+)
+
+export default ContactPage
